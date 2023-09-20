e880dd6b446ba3b0e90e0a8286b43a549ee6ff48
Dancomey <comeydan@gmail.com>
2023-09-20 17:14:50 -0400

    updated ParetoChartCreatorTool
    added CreatePareto function

--- ParetoChartCreatorTool.py
+++ ParetoChartCreatorTool.py
@@ -5,72 +5,88 @@
 
 import pandas as pd 
 import numpy as np 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import json
+import random
 
 
-data = input("Please enter something: ")
-print("You entered:", data)
+def CreatePareto(df, column_name, axis_label, chart_title):
+    #sorting the values of the dataframe in descending order
+    df.sort_values(by= column_name, ascending= False, inplace= True)
+
+
+    #creating the histogram portion of the chart
+
+    fig, ax = plt.subplots(figsize= (16, 9))
+
+    ax.bar(x= df.index, height= df[column_name], width = 0.98, edgecolor = "Black")
+
+    ax.set_ylim(0, int(1.1*df[column_name].max()))
+    ax.set_yticks(range(0, int(1.1*df[column_name].max()), 5))
+
+    ax.set_title(chart_title, fontsize= 28)
+
+    ax.set_ylabel(axis_label, fontsize= 18)
+
+    #creating the line portion of the chart
+
+    #new column for the cumulative sum of the (sorted) data
+    df["cumsum"] = df[column_name].cumsum()
+
+    #new column for the relative cumulative sum
+    df["relcumsum"] = df["cumsum"] / df[column_name].sum()
+
+
+    #adding second Y-axis for the line plot
+    ax2 = ax.twinx()
+    ax2.plot(df["relcumsum"], marker = "o", linewidth = 3.0, markersize = 6.0, color = "black")
+
+    ax2.set_ylabel("Cumulative Percentage", fontsize= 18)
+
+
+    ax.spines['top'].set_visible(False)
+    ax2.spines['top'].set_visible(False)
+
+
+    for x_val, y_val in zip(range(len(df)),df["relcumsum"]):
+        text = f"{y_val:.0%}"
+        ax2.text(x = x_val - 0.10, y = y_val + 0.015, s = text, color = "black", ha = "center", va = "center")
+
+
+    ax2.axhline(y=0.8, color='red', linestyle='--')
+
+    plt.savefig("CustomChart2.png")
 
 
 
+# user_data = input("Please enter data in dictionary form: ")
+# column_name = input("Please enter the column name: ")
+# axis_label = input("Please enter the axis label: ")
+# chart_title = input("Please enter the chart title: ")
+
+column_name = "State_Sales"
+axis_label = "Sales ($)"
+chart_title = "Test with random data"
+
+# dict_data = json.loads(user_data)
+
+dict_data = test_data = {
+    "Arkansas": random.randint(1, 100),
+    "Texas": random.randint(1, 100),
+    "Maine": random.randint(1, 100),
+    "Georgia": random.randint(1, 100),
+    "Alaska": random.randint(1, 100),
+    "Florida": random.randint(1, 100),
+    "Vermont": random.randint(1, 100)
+}
+
+
+#Creating a dataframe with the above dictionary
+df = pd.DataFrame.from_dict(dict_data, orient= "index",columns= [column_name])
+
+CreatePareto(df, column_name, axis_label, chart_title)
 
 
 
-
-
-# #Dictionary of sales by state
-# sales_dict = {"Arkansas": 10, "Texas": 8, "Maine": 3, "Georgia": 5, "Alaska": 1, "Florida": 20, "Vermont": 2, "California": 1}
-
-# #Creating a dataframe with the above dictionary
-# df = pd.DataFrame.from_dict(sales_dict, orient= "index",columns= ["sales_per_state"])
-# #print(df.head())
-
-# #sorting the values of the dataframe in descending order
-# df.sort_values(by= "sales_per_state", ascending= False, inplace= True)
-
-
-# #creating the histogram portion of the chart
-
-# fig, ax = plt.subplots(figsize= (16, 9))
-
-# ax.bar(x= df.index, height= df["sales_per_state"], width = 0.98, edgecolor = "Black")
-
-# ax.set_ylim(0, 22)
-# ax.set_yticks(range(0, 24, 2))
-
-# ax.set_title("Weekly Sales by State", fontsize= 28)
-
-# ax.set_ylabel("Sales ($)", fontsize= 18)
-
-# # plt.savefig("histogram1.png")
-
-
-# #creating the line portion of the chart
-
-# #new column for the cumulative sum of the (sorted) data
-# df["cumsum"] = df["sales_per_state"].cumsum()
-
-# #new column for the relative cumulative sum
-# df["relcumsum"] = df["cumsum"] / df["sales_per_state"].sum()
-
-
-# #adding second Y-axis for the line plot
-# ax2 = ax.twinx()
-# ax2.plot(df["relcumsum"], marker = "o", linewidth = 3.0, markersize = 6.0, color = "black")
-
-# ax2.set_ylabel("Cumulative Percentage", fontsize= 18)
-
-
-# ax.spines['top'].set_visible(False)
-# ax2.spines['top'].set_visible(False)
-
-
-# for x_val, y_val in zip(range(len(df)),df["relcumsum"]):
-# 	text = f"{y_val:.0%}"
-# 	ax2.text(x = x_val - 0.10, y = y_val + 0.015, s = text, color = "black", ha = "center", va = "center")
-
-
-# ax2.axhline(y=0.8, color='red', linestyle='--')
-
-# plt.savefig("histogram3.png")
+    
 
